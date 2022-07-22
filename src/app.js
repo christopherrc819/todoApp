5fcee9ea078b8866070ac9a609509bd68e37369e
Chris Wolf <christopherc819@gmail.com>
2022-07-22 18:55:02 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -146,10 +146,15 @@ document.addEventListener('DOMContentLoaded', () => {
     todoListSection.innerHTML = '';
     todoArray.forEach((todoList, index) => {
       const listItem = document.createElement('li');
+      
+      if (currentTheme == 'light') {
+        listItem.setAttribute('class', 'todoItem draggable lightTheme');
+      } else if (currentTheme == 'dark') {
+        listItem.setAttribute('class', 'todoItem draggable');
+      }
       listItem.setAttribute('data-id', todoList.id);
       listItem.setAttribute('data-index', index + 1);
       listItem.setAttribute('draggable', true);
-      listItem.setAttribute('class', 'todoItem draggable')
       // listItem.addEventListener('dragstart', () => {
       //   listItem.classList.add('dragging');
       //   console.log('dragstart')
@@ -162,11 +167,7 @@ document.addEventListener('DOMContentLoaded', () => {
       // listItem.addEventListener('drop', dragDrop);
       // listItem.addEventListener('dragleave', dragLeave);
 
-      // if (currentTheme == 'light') {
-      //   listItem.setAttribute('class', 'todoItem draggable lightTheme');
-      // } else if (currentTheme == 'dark') {
-      //   listItem.setAttribute('class', 'todoItem draggable');
-      // }
+
       if (todoList.completed == true) {
         //Toggle Check Mark Section
         const checkMarkImg = document.createElement('img');
